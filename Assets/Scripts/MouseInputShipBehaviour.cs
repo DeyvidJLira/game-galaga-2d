5c0ccffd4d456ade8c567d2fab9708c2a9faa187
Deyvid Jaguaribe de Lira <deyvidjlira@gmail.com>
2019-09-01 11:18:19 -0300

    Create Ship and MouseInputShipBehaviour script

---
+++ Assets/Scripts/MouseInputShipBehaviour.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseInputShipBehaviour : MonoBehaviour {
+    // Update is called once per frame
+    void Update() {
+        transform.position = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, transform.position.y);
+    }
+}
