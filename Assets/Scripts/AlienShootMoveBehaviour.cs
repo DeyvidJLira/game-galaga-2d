79f8b7ae1b1911d92eb258c4528873821a7fe215
Deyvid Jaguaribe de Lira <deyvidjlira@gmail.com>
2019-09-06 23:44:54 -0300

    Create AlienA / Prepare Movement of AlienA and AlienB / Create Shoot Alien

---
+++ Assets/Scripts/AlienShootMoveBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlienShootMoveBehaviour : MonoBehaviour
+{
+    [SerializeField] protected GameObject projectilePrefab;
+    [SerializeField] protected float projectileSpeed;
+    [SerializeField] protected Transform alienBody;
+
+    protected GameObject projectileTemp;
+
+    public void shoot() {
+        projectileTemp = Instantiate(projectilePrefab);
+        projectileTemp.GetComponent<ProjectileBehaviour>().Shoot(alienBody.position, Vector2.down, projectileSpeed);
+    }
+}
---
+++ Assets/Scripts/AlienSimpleMoveBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlienSimpleMoveBehaviour : MonoBehaviour
+{
+    [SerializeField] Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("flyMove", Random.Range(5,10));
+    }
+
+   protected void flyMove() {
+        animator.SetTrigger("attack");
+
+        Invoke("flyMove", Random.Range(5, 10));
+    }
+}
