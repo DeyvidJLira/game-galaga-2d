dd49af0f4b68452d1541591bc172ed53a09bef35
Deyvid Jaguaribe de Lira <deyvidjlira@gmail.com>
2019-09-15 14:59:34 -0300

    Create system of Life and Score / Create animation of hit for Ship / Prepare for GameOver

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -9,12 +10,29 @@ public class GameManager : MonoBehaviour
 
     public enum AlienType { ALIEN_ZAKO, ALIEN_GOEI, ALIEN_BOSS}
 
+    [Header("Waves")]
     [SerializeField] protected Transform formationContent;
     [SerializeField] protected GameObject[] alienFormations;
     [SerializeField] protected GameObject[] alienKinds;
 
     protected int activeStage = 0;
     protected int aliensCount = 0;
+
+    [Header("Life")]
+    [SerializeField] protected GameObject ship;
+    [SerializeField] protected GameObject lifeIcon;
+    [SerializeField] protected RectTransform lifeContainer;
+    [Range(1, 3)]
+    [SerializeField] protected int initialLife = 3;
+
+    protected int life = 1;
+    protected int maxLife = 3;
+
+    [Header("Score")]
+    [SerializeField] protected Text scoreText;
+    [SerializeField] protected int pointsAlien = 10;
+
+    protected int score = 0;
 
     private void Awake() {
         if (instance == null) {
@@ -27,6 +45,8 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        life = initialLife;
+        updateLifeUI();
         GameObject currentFormation = MountAlienFormation(activeStage);
         CreateAliensInFormation(currentFormation.transform);
     }
@@ -34,6 +54,8 @@ public class GameManager : MonoBehaviour
     private void Update() {
 
     }
+
+    #region Waves
 
     protected GameObject MountAlienFormation(int activeFormation) {
         //Create the first formation of the list
@@ -66,8 +88,19 @@ public class GameManager : MonoBehaviour
         }
     }
 
-    public void onShipHit(Collider2D collider) {
+    #endregion
 
+    #region Collisions
+
+    public void onShipHit(Collider2D collider) {
+        Destroy(collider.gameObject);
+        ship.GetComponent<Animator>().SetTrigger("hit");
+
+        if (life == 0) {
+            Time.timeScale = 0;
+        }
+
+        decreaseLife();
     }
 
     public void onAlienHit(GameObject alien, Collider2D collider) {
@@ -78,6 +111,8 @@ public class GameManager : MonoBehaviour
 
         aliensCount--;
 
+        increaseScore(pointsAlien);
+
         if(aliensCount == 0) {
             Destroy(formationContent.GetChild(0).gameObject);
             activeStage++;
@@ -85,5 +120,49 @@ public class GameManager : MonoBehaviour
         }
 
     }
+
+    #endregion
+
+    #region Life
+
+    private void updateLifeUI() {
+        for(int i = 0; i < life; i++) {
+            GameObject lifeUI = Instantiate(lifeIcon);
+            lifeUI.transform.SetParent(lifeContainer);
+            lifeUI.transform.localScale = Vector3.one;
+        }
+    }
+
+    private void decreaseLife() {
+        if(life > 0) {
+            life--;
+            Destroy(lifeContainer.GetChild(0).gameObject);
+        }
+    }
+
+    #endregion
+
+    #region Score
+
+    private void increaseScore(int points) {
+        score += points;
+        scoreText.text = maskPoints(score);
+    }
+
+    private string maskPoints(double points) {
+        int count = 5;
+        while ((points /= 10.0) >= 1)
+            count--;
+        string value = "";
+        do {
+            value += "0";
+            count--;
+        } while (count > 0);
+        value += score.ToString();
+        value += " ";
+        return value;
+    }
+
+    #endregion
 
 }
